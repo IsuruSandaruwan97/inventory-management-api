45d1468a6bca68b06284f07fdd49b8114740d01e
Isuru Sandaruwan <isuruskarunarathne@gmail.com>
2024-09-03 20:32:21 +0530

    feat: Implement product sub-categories

--- src/modules/categories/categories.controller.ts
+++ src/modules/categories/categories.controller.ts
@@ -4,15 +4,15 @@ import { CategoryService } from '@modules/categories/services/category.service';
 import { ApiRequest, ApiResponseType } from '@configs/types/api-response.type';
 import { CreateCategoryDto } from '@modules/categories/dto/create-category.dto';
 import { UpdateCategoriesDto } from '@modules/categories/dto/update-category.dto';
-import { SubCategoryService } from '@modules/categories/services/sub-category.service';
-import { CreateSubCategoryDto } from '@modules/categories/dto/create-sub-category.dto';
+import { ApiTags } from '@nestjs/swagger';
 
 
 @Controller('categories')
+@ApiTags('Categories')
 export class CategoriesController {
 constructor( private readonly responseHandlerService: ResponseHandlerService,
              private readonly categoryService:CategoryService,
-             private readonly subCategoryService:SubCategoryService) {
+             ) {
 }
   @Get()
   async categories():Promise<ApiResponseType> {
@@ -29,13 +29,5 @@ constructor( private readonly responseHandlerService: ResponseHandlerService,
     return this.responseHandlerService.successResponse(await this.categoryService.updateCategory(body, req.user.id))
   }
 
-  @Get('sub')
-  async subCategories(@Req() req:ApiRequest):Promise<ApiResponseType> {
-    return this.responseHandlerService.successResponse(await this.subCategoryService.fetchSubCategories())
-  }
 
-  @Post('sub')
-  async createSubCategory(@Body() body:CreateSubCategoryDto,@Req() req:ApiRequest):Promise<ApiResponseType> {
-    return this.responseHandlerService.successResponse(await this.subCategoryService.create(body,req.user.id))
-  }
 }
--- src/modules/categories/subcategories.controller.ts
+++ src/modules/categories/subcategories.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Req } from '@nestjs/common';
+import { ApiRequest, ApiResponseType } from '@configs/types/api-response.type';
+import { CreateSubCategoryDto } from '@modules/categories/dto/create-sub-category.dto';
+import { ResponseHandlerService } from '@services/response-handler.service';
+import { SubCategoryService } from '@modules/categories/services/sub-category.service';
+import { UpdateSubCategoriesDTO } from '@modules/categories/dto/update-sub-category';
+import { ApiTags } from '@nestjs/swagger';
+import { FetchSubCategoriesDTO } from '@modules/categories/dto/fetch-sub-categories.dto';
 
 @Controller('subcategories')
-export class SubcategoriesController {}
+@ApiTags('SubCategories')
+export class SubcategoriesController {
+constructor(private readonly responseHandlerService: ResponseHandlerService,
+            private readonly subCategoryService: SubCategoryService) {
+}
+  @Get(':code')
+  async fetchSubCategories(@Param() params:FetchSubCategoriesDTO):Promise<ApiResponseType> {
+    return this.responseHandlerService.successResponse(await this.subCategoryService.fetchSubCategories(params.code))
+  }
+
+  @Post()
+  async createSubCategory(@Body() body:CreateSubCategoryDto,@Req() req:ApiRequest):Promise<ApiResponseType> {
+    return this.responseHandlerService.successResponse(await this.subCategoryService.create(body,req.user.id))
+  }
+
+  @Put()
+  async updateSubCategory(@Req() req:ApiRequest, @Body() body:UpdateSubCategoriesDTO):Promise<ApiResponseType> {
+  return this.responseHandlerService.successResponse(await this.subCategoryService.updateById(body,req.user.id))
+  }
+}
